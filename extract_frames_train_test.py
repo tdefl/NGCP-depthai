31ea5109b55cd48b7561533dd59ad21b055687f5
Alfred Tran <traderflan@gmail.com>
2024-03-01 00:41:24 -0800

    extract with supervision

--- extract_frames_train_test.py
+++ extract_frames_train_test.py
@@ -15,4 +15,4 @@ for video_path in tqdm(TRAIN_VIDEO_PATHS):
     image_name_pattern = video_name + "-{:05d}.png"
     with sv.ImageSink(target_dir_path=IMAGE_DIR_PATH, image_name_pattern=image_name_pattern) as sink:
         for image in sv.get_video_frames_generator(source_path=str(video_path), stride=FRAME_STRIDE):
-            sink.save_image(image=image)
+            sink.save_image(image=image) 
