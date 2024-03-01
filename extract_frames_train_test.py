e44db55bcc61260c7882ca7bf103dffcaefd8802
AlfredTran <traderflan@gmail.com>
2024-03-01 01:07:28 -0800

    extracted every 10th frame into images/

--- extract_frames_train_test.py
+++ extract_frames_train_test.py
@@ -1,36 +1,28 @@
-# sudo apt-get install libgl1-mesa-glx
-# this fixed missing library issue on ubuntu wsl. however macbook is still failing and saying 
-# supervision module cannot be found/resolved
-# i pip installed supervision on mac, and itg is listed. perhaps i need to add toi the PATH?
 import os
 import supervision as sv
 from tqdm import tqdm
-import gdown  # for downloading from google drive
 
-# Get the home directory dynamically
-HOME = os.path.expanduser('~')
-
-# get current working directory
+# Get the current working directory
 CURRENT_DIR = os.getcwd()
 
-# Local directory where you want to save the downloaded video file
-#LOCAL_VIDEO_DIR = f"{HOME}/videos"
-LOCAL_VIDEO_DIR = f"{CURRENT_DIR}/videos"
+# Local path to the downloaded video file
+LOCAL_VIDEO_PATH = f"{CURRENT_DIR}/red_square.mp4"
 
-# Google Drive link to the video file
-GOOGLE_DRIVE_VIDEO_LINK = "https://drive.google.com/file/d/1hNCXtBr7XjTzV2uGNzDGgI4fuov1Y2bb/view?usp=sharing"
+# Ensure the video file exists
+if not os.path.exists(LOCAL_VIDEO_PATH):
+    raise FileNotFoundError(f"The video file '{LOCAL_VIDEO_PATH}' does not exist.")
 
-# Download the video file using gdown
-gdown.download(GOOGLE_DRIVE_VIDEO_LINK, f"{LOCAL_VIDEO_DIR}/video.mp4", quiet=False)
+# List containing the local path to the video file
+VIDEO_PATHS = [LOCAL_VIDEO_PATH]
 
-# List the local directory to get the video path
-video_paths = sv.list_files_with_extensions(directory=LOCAL_VIDEO_DIR, extensions=["mp4"])
-
-TEST_VIDEO_PATHS, TRAIN_VIDEO_PATHS = video_paths[:2], video_paths[2:]
+# Directory where you want to save the extracted images
 IMAGE_DIR_PATH = f"{CURRENT_DIR}/images"
+
+# Frame extraction parameters
 FRAME_STRIDE = 10
 
-for video_path in tqdm(TRAIN_VIDEO_PATHS):
+# Loop through the video paths and extract frames
+for video_path in tqdm(VIDEO_PATHS):
     video_name = os.path.splitext(os.path.basename(video_path))[0]
     image_name_pattern = video_name + "-{:05d}.png"
     with sv.ImageSink(target_dir_path=IMAGE_DIR_PATH, image_name_pattern=image_name_pattern) as sink:
