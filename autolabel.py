4bfd8e534f2ff961f2b4b0b714ce35026f6f0fb0
AlfredTran <traderflan@gmail.com>
2024-03-01 01:22:42 -0800

    update autolabel.py. not sure if works yet

---
+++ autolabel.py
@@ -0,0 +1,20 @@
+from autodistill.detection import CaptionOntology
+from autodistill_grounded_sam import GroundedSAM
+import os
+ontology = CaptionOntology({
+    "red square" : None
+})
+
+CURRENT_DIR = os.getcwd()
+DATASET_DIR_PATH = os.path.join(CURRENT_DIR, "dataset")
+os.makedirs(DATASET_DIR_PATH, exist_ok = True)
+os.makedirs(DATASET_DIR_PATH, exist_ok = True)
+
+# run base model.
+base_model = GroundedSAM(ontology=ontology)
+dataset = base_model.label(
+    input_folder="images/",
+    extension=".png",
+    output_folder=DATASET_DIR_PATH)
+
+
